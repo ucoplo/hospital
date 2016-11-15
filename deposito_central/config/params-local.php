46669dc6d5de99701483971f6123d3cf747a2458
ulises <ulicoplo@gmail.com>
2016-11-15 10:42:51 -0300

    faltantes en deposito

---
+++ deposito_central/config/params-local.php
@@ -0,0 +1,8 @@
+<?php
+return [
+ 'servicio' => '005', //Depósito central
+ 'depositos_central' => ['01','53'], // DEpositos habilitados 
+ 'CodModulo' => '213', // es para seguir la lógica del Hospital
+ 'servicios_quirofano' => ['039','019'],//servicios en los cuales se fracciona en quirofano
+ 'servicio_farmacia' => '021', //Se utiliza para generarle Remito para ser importado desde Farmacia
+];
